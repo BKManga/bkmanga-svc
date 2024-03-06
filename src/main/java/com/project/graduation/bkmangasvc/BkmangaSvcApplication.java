492ce68a2f91b2716b4f30f72862fabb1347814d
nghiann@zyyx.jp <nghiann@zyyx.jp>
2024-03-06 16:06:31 +0700

    Init project

---
+++ src/main/java/com/project/graduation/bkmangasvc/BkmangaSvcApplication.java
@@ -0,0 +1,13 @@
+package com.project.graduation.bkmangasvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkmangaSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkmangaSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/graduation/bkmangasvc/BkmangaSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.graduation.bkmangasvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkmangaSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
